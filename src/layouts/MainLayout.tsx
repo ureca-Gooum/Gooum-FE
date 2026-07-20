cc09e2cf7442df5f9ca09defab0b2005f7f69efb
Ppakso <apfkzl9060@gmail.com>
2026-07-20 15:12:36 +0900

    feat: 공통 레이아웃 설정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/components/layout/Header';
+import { Sidebar } from '@/components/layout/Sidebar';
 
 export const MainLayout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="flex h-screen flex-col overflow-hidden bg-bg-canvas ">
+      <Header />
+      <div className="flex flex-1 overflow-hidden pt-4 pb-2.5 pr-2.5 gap-4">
+        <Sidebar />
+        <Outlet />
+      </div>
     </div>
   );
 };
